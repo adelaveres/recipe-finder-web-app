7251efcb188d7e363c2c069c4baca4b8b857757d
Adela Veres <adela.veres@yahoo.ro>
2024-10-02 15:04:22 +0300

    Initial SearchBar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+
+const SearchBar = ({onSearch}) => {
+    const [query, setQuery] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onSearch(query);
+        setQuery('');
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className="flex mb-4">
+            <input
+                type="text"
+                value={query}
+                onChange={(e)=>setQuery(e.target.value)}
+                placeholder='What do you want to eat?'
+                className="flex-grow p-2 border rounded"
+            />
+            <button type="submit" className="ml-2 p-2 bg-blue-500 text-white rounded">
+                Search
+            </button>
+        </form>
+    );
+};
+
+export default SearchBar;
