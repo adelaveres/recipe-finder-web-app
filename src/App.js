d7f5e47b59fe189138c75ff11395771121d14191
Adela Veres <adela.veres@yahoo.ro>
2024-10-02 14:49:43 +0300

    Created Routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Favorites from './pages/Favorites';
+import useFavorites from './hooks/useFavorites';
 
 function App() {
+
+  const {favorites, addFavorite, removeFavorite} = useFavorites();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Home addFavorite={addFavorite}/>
+        </Route>
+        <Route path="/favorites">
+          <Favorites favorites={favorites} removeFavorite={removeFavorite} />
+        </Route>
+        
+      </Switch>
+    </Router>
   );
 }
 
